9fe15f44b34e2bb29eec96a8ddff9385ef53705f
Gagan-09 <gaganmshetty09@gmail.com>
2026-06-27 19:48:30 +0530

    Updates

--- assets/swimlanesDiagram-G3AALYLV-Cndc82GQ.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-BTGnZz5C.js";import{_ as a}from"./index-DLP9Ihny.js";import"./chunk-5VM5RSS4-DlJIqbie.js";import"./chunk-XXDRQBXY-CIF_V0A6.js";import"./chunk-VR4S4FIN-Cf2H1AgO.js";import"./chunk-32BRIVSS-CmAPhAKh.js";import"./channel-CkoWvPa-.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
